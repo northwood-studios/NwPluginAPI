3896a940a75e2e8f603d58d755d7e2001ddd638a
Łukasz Jurczyk <zabszk@protonmail.ch>
2022-12-13 21:01:20 +0100

    Version bump

--- NwPluginAPI/PluginApiVersion.cs
+++ NwPluginAPI/PluginApiVersion.cs
@@ -3,7 +3,7 @@
 	public static class PluginApiVersion
 	{
 		public const string Version = "12.0.0"; //major.minor.patch ONLY
-		public const string VersionString = "12.0.0-rc.1";
+		public const string VersionString = "12.0.0-rc.2";
 		
 		//PackageVersion needs to be set to the same value as VersionString MANUALLY IN .csproj
 	}
