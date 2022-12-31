d423b646307426e572be13c7d3bbb01a1b8d720a
SrLicht <francoquiroz_13@live.com>
2022-12-31 18:47:01 -0300

    I forgot about the Analyzer

--- Analyzers/NWPluginAPI.Analyzers/Generated/GeneratedEventManager.cs
+++ Analyzers/NWPluginAPI.Analyzers/Generated/GeneratedEventManager.cs
@@ -397,5 +397,9 @@ public static class EventManager
 			new EventParameter("PluginAPI.Core.Interfaces.IPlayer", false, "player"),
 			new EventParameter("InventorySystem.Items.ItemBase", false, "item"),
 			new EventParameter("Scp914.Scp914KnobSetting", false, "knobSetting")) },
+		{ 114, new Event(
+			new EventParameter("PluginAPI.Core.Interfaces.IPlayer", false, "player"),
+			new EventParameter("Scp914.Scp914KnobSetting", false, "knobSetting"),
+			new EventParameter("UnityEngine.Vector3", false, "outPosition")) },
 	};
 }
