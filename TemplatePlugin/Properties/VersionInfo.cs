5c18ad24c16775e356d65a16c3608fab33cf1706
Łukasz Jurczyk <zabszk@protonmail.ch>
2023-01-15 14:56:07 +0100

    Minor adjustments and fixes

---
+++ TemplatePlugin/Properties/VersionInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyInformationalVersion(PluginAPI.PluginApiVersion.VersionString)]
+[assembly: AssemblyFileVersion(PluginAPI.PluginApiVersion.VersionString)]
+[assembly: AssemblyVersion(PluginAPI.PluginApiVersion.Version)]
