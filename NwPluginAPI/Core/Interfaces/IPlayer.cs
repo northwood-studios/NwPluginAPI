6583ad4076d6048650029bcf178bf2acde4acd67
ced777ric <44529860+ced777ric@users.noreply.github.com>
2023-06-26 18:02:29 +0200

    IsOffline (#207)

--- NwPluginAPI/Core/Interfaces/IPlayer.cs
+++ NwPluginAPI/Core/Interfaces/IPlayer.cs
@@ -5,5 +5,24 @@ namespace PluginAPI.Core.Interfaces
 	/// </summary>
 	public interface IPlayer : IEntity
 	{
+		/// <summary>
+		/// Gets if the player is in the server.
+		/// </summary>
+		bool IsOffline { get; }
+		
+		/// <summary>
+		/// Gets the player's user id.
+		/// </summary>
+		string UserId { get; }
+		
+		/// <summary>
+		/// Gets the player's name.
+		/// </summary>
+		string Nickname { get; }
+		
+		/// <summary>
+		/// Gets the player's ip address.
+		/// </summary>
+		string IpAddress { get; }
 	}
 }
